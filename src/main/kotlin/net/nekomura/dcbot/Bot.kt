2cb270d661c5a032ba87bf75e0cf7df609a62a05
Huanying04 <ken911230@gmail.com>
2020-10-11 13:53:59 +0800

    Little Update

--- src/main/kotlin/net/nekomura/dcbot/Bot.kt
+++ src/main/kotlin/net/nekomura/dcbot/Bot.kt
@@ -15,18 +15,31 @@ import java.util.concurrent.TimeUnit
  * 使用之前請先將config.json中的TOKEN、OWNER、PIXIV_PUSH_NOTIFICATION_CHANNEL、PIXIV_R18_PUSH_NOTIFICATION_CHANNEL、MINECRAFT_PUSH_NOTIFICATION_CHANNEL、MINECRAFT_UPDATE_CHECKER、PIXIV_UPDATE_CHECKER、FOLLOW_PIXIV設定好
  */
 fun main(args: Array<String>) {
-    if (Config.get(ConfigStringData.TOKEN)!!.isEmpty()) {
+    if (Config.getConfig().getBoolean("first_use")) {
         val scanner = Scanner(System.`in`)
+        if (Config.get(ConfigStringData.TOKEN).isNullOrEmpty()) {
+            val config = Config.getConfig()
+            print("請輸入你的Discord機器人的Token: ")
+            config.put(ConfigStringData.TOKEN.toString(), scanner.next())
+            Config.writeConfig(config.toString())
+        }
+        if (Config.get(ConfigLongData.OWNER) == 0L) {
+            val config = Config.getConfig()
+            print("請輸入你的Discord帳號ID: ")
+            config.put(ConfigLongData.OWNER.toString(), scanner.nextLong())
+            Config.writeConfig(config.toString())
+        }
+        if (Config.get(ConfigStringData.PIXIV_PHPSESSID).isNullOrEmpty()) {
+            val config = Config.getConfig()
+            print("請輸入你pixiv的PHPSESSID(沒有的話直接Enter就行): ")
+            config.put(ConfigStringData.PIXIV_PHPSESSID.toString(), scanner.nextLine())
+            Config.writeConfig(config.toString())
+        }
+
+        scanner.close()
+
         val config = Config.getConfig()
-        print("請輸入你的Discord機器人的Token: ")
-        config.put(ConfigStringData.TOKEN.toString(), scanner.next())
-        Config.writeConfig(config.toString())
-    }
-    if (Config.get(ConfigLongData.OWNER) == 0L) {
-        val scanner = Scanner(System.`in`)
-        val config = Config.getConfig()
-        print("請輸入你的Discord帳號ID: ")
-        config.put(ConfigLongData.OWNER.toString(), scanner.nextLong())
+        config.put("first_use", false)  //下次開啟時不再詢問
         Config.writeConfig(config.toString())
     }
 
