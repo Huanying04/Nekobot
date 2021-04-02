2001abfebf58e132a0eedea22fa6efcf66ce1c1c
Huanying04 <ken911230@gmail.com>
2021-04-02 19:06:17 +0800

    新增頁碼檢測

--- src/main/java/net/nekomura/dcbot/commands/PixivIllustrationCommand.java
+++ src/main/java/net/nekomura/dcbot/commands/PixivIllustrationCommand.java
@@ -23,7 +23,13 @@ public class PixivIllustrationCommand implements ICommand {
             eb.setDescription("該作品為R18作品，請在限制級頻道裡執行此指令或使用其他作品ID");
             ctx.event.getChannel().sendMessage(eb.build()).queue();
         }else {
-            byte[] image = info.getImage(Integer.parseInt(ctx.args.get(1)), PixivImageSize.Original);
+            int page = Integer.parseInt(ctx.args.get(1));
+            boolean announcement = false;
+            if (Integer.parseInt(ctx.args.get(1)) >= info.getPageCount()) {
+                page = 0;
+                announcement = true;
+            }
+            byte[] image = info.getImage(page, PixivImageSize.Original);
 
             eb.setImage("attachment://" + info.getId() + "." + info.getImageFileFormat(0));
             eb.addField("標題", info.getTitle(), false);
@@ -31,8 +37,11 @@ public class PixivIllustrationCommand implements ICommand {
             eb.addField("標籤", Arrays.toString(info.getTags()), false);
             eb.addField("作者", "[" + info.getAuthorName() + "](https://www.pixiv.net/users/" + info.getAuthorID() + ")", false);
             eb.addField("ID", "[" + ctx.args.get(0) + "](https://www.pixiv.net/artworks/" + ctx.args.get(0) + ")", true);
-            eb.addField("頁碼", ctx.args.get(1), true);
+            eb.addField("頁碼", String.valueOf(page), true);
             eb.addField("頁數", String.valueOf(info.getPageCount()), true);
+            if (announcement) {
+                eb.addField("提醒", "您輸入的頁碼`" + ctx.args.get(1) + "`大於或等於本作品總頁數(初始頁由`0`算起)`" + info.getPageCount() + "`。故將使用第`0`頁代替。", false);
+            }
             ctx.event.getChannel().sendFile(image, info.getId() + "." + info.getImageFileFormat(0)).embed(eb.build()).queue();
         }
     }
